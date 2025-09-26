10b9868227ddeab247859d1a74b38a66b90df4e9
dearzubi <zubairkhalidce@gmail.com>
2025-09-26 23:02:33 +0100

    refactor: update server.js file to reflect previous refactoring changes

--- src/server.js
+++ src/server.js
@@ -2,15 +2,13 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import * as process from "node:process";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./swagger/swaggerConfig.js";
-
-import userRouter from "./routes/userRoute.js";
-import forgotPasswordRouter from "./routes/forgotPassword.js";
-import shiftsRouter from "./routes/shiftsRoute.js";
-
-//app config
+import authRouter from "./routes/authentication.router.js";
 dotenv.config();
+//app config
+
 const app = express();
 const port = process.env.PORT || 8001;
 mongoose.set("strictQuery", true);
@@ -20,24 +18,16 @@ app.use(express.json());
 app.use(cors());
 
 //db config
-mongoose.connect(
-  process.env.MONGO_URI,
-  {
+mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-  },
-  (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log("DB Connected");
-    }
-  },
-);
+  })
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB connection error: ", err));
 
 //api endpoints
-app.use("/api/user", userRouter);
-app.use("/api/forgotPassword", forgotPasswordRouter);
-app.use("/api/shifts", shiftsRouter);
+app.use("/api/user", authRouter);
+// app.use("/api/shifts", shiftsRouter);
 // Swagger Docs
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
